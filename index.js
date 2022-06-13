5dc3b17975995e11541300a915d34d2e4cd850e0
Kipleting <kipletingedwin4@gmail.com>
2022-06-13 14:05:42 +0300

    commit tree 354cd0e0399bc35004bf718cd40ed1621dd64e22

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //Adding an Event Listerner
-
-
-function clickAlert() {
-  alert("I was clicked");
+function clickMe(){
+  alert ("I was clicked!");
 }
-function addingEventListener() {
-  const input = document.getElementById("input");
-  input.addEventListener("click", clickAlert);
-}
+ function addingEventListener (){
+   const input =document.getElementById("input"); 
+   input.addEventListener("click",clickMe);
+ }
+
 
 
